544d0dd6617f46f4679a705e49f41e2491e4ef43
Alejandra Pinto <copinto-@student.42.us.org>
2020-05-05 00:41:16 +0200

    C

---
+++ Course C/ex6v10.c
@@ -0,0 +1,16 @@
+/*Arkanabytes 2017*/
+//operador de asignaciones 
+#include<stdio.h>
+int main(){
+	int a,b,c;
+//	a=b=c=10;
+	a = 10;
+//	a = a + 10;
+//	a +=10; // a = a +10
+//	a -=5; // a = a -5
+//	a*=2; // a = a * 2
+	a /=2; // a= a /2
+	
+	printf("El valor de A es: %i ",a);
+	return 0;
+}
