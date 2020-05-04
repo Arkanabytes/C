ead76596c959ad2fb551a14db913e8f713941e25
Alejandra Pinto <copinto-@student.42.us.org>
2020-05-04 19:55:51 +0200

    C

---
+++ Course C/ex1.cpp
@@ -0,0 +1,9 @@
+//Curso1
+//Alejandra Pinto 2017
+
+#include <stdio.h> //cabecera estandart de entrada y salida de datos h header es de cabecera
+int main(){ //funcion entero principal
+	
+	printf("hola ALEJANDRA\n");
+	return 0;
+}
