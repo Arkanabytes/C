fafc1c397bc04a056abf0d9da938d2585410dd01
Alejandra Pinto <copinto-@student.42.us.org>
2020-05-06 01:36:10 +0200

    C

---
+++ Course C/ex10v14respuesta.c
@@ -0,0 +1,16 @@
+/*Arkanabytes-2017*/
+// Dadas las horas trabajadas de una persona calculo el salario minimo e imprimirlo
+#include<stdio.h>
+int main(){
+	int tra,horas,valor,sal_final;
+	printf("\t\tCalculador de salario");
+	printf("\n\nIngrese la horas que ha trabajado: ",&horas);
+	scanf("%i",&horas);
+	tra=horas*valor;
+	sal_final=tra;
+	printf("\nEl resultado del salario minimo es: %i",&sal_final);
+	printf("\n\nAutor: Alejandra Pinto");
+	
+	return 0;
+}
+
