e7d96c15a9185f04cd9caa17f93eb6dac3e55201
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-05-08 20:52:54 +0200

    C

--- Course C/ex33v33.c
+++ Course C/ex33v33.c
@@ -1,35 +1,4 @@
-/*el bucle for:
-
-Sintaxis:
-			for(inicializacion; condicion ; incremento){
-			Instrucciones;
-			...
-			}
-*/
-/*
-//ascendete
-#include<stdio.h>
-int main(){
-	int num,resultado;
-	printf("\t\nMuestra los primeros 10 numeros\n");
-		for(num=1;num<=10;num++){
-			printf("%i\t",num);
-		}
-	return 0;
-}*/
-
-//descendente
-/*
-#include<stdio.h>
-int main(){
-	int num,resultado;
-	printf("\t\nMuestra los primeros 10 numeros\n");
-		for(num=10;num>=1;num--){
-			printf("%i\t",num);
-		}
-	return 0;
-}*/
-
+/*Arkanabytes*/
 #include<stdio.h>
 int main(){
 	int num,resultado;
