5e6de14d3c549fb42494a9765b2b65e3768d9c38
Alejandra Pinto <copinto-@student.42.us.org>
2020-12-12 23:30:06 +0100

    .

--- Course C/ex31v31respuesta.c
+++ Course C/ex31v31respuesta.c
@@ -7,8 +7,8 @@ int main(){
 	printf("\tMultiplos a comprobar");
 	printf("\n\tDigite el total de numeros a comprobar: "); //ingresa el usuario los numeros
 	scanf("%i",&n);// guardo la informacion
-	i=1; // lo inicializo la i con un uno
-	while(i<=n){ // mientras que i menor o igual a n
+		i=1; // lo inicializo la i con un uno
+		while(i<=n){ // mientras que i menor o igual a n
 		if(i%5==0){// si su i lo iria sumando pero con el mod lo divido para encontrar el residuo es igual a cero  3 igual cero
 			printf("\t%i",i);	
 			}
