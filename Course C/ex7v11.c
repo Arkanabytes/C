3a69c26d450a005ac4af4f40a41f7356ceff2636
Alejandra Pinto <copinto-@student.42.us.org>
2020-12-12 23:34:31 +0100

    .

--- Course C/ex7v11.c
+++ Course C/ex7v11.c
@@ -1,5 +1,6 @@
-//*1. Pedir 2 numeros al usuario y sumarlos, restarlos,multiplicarlos y dividirlos
 /*Arkanabytes 2017*/
+
+//*1. Pedir 2 numeros al usuario y sumarlos, restarlos,multiplicarlos y dividirlos
 #include<stdio.h>
 	int main(){
 				int n1,n2,suma,resta,multiplicacion,division;
